a35b3eb607193e76f2eadf0393f56e05c9d83062
Nlapiere <33150220+Nlapiere@users.noreply.github.com>
2018-08-29 11:48:43 +0200

    Update utilitaire.h

--- utilitaire.h
+++ utilitaire.h
@@ -6,3 +6,25 @@ void practice(int index, int p);
  * Créer une feuille de résultats
  */
 void generateRecapFilePractice();
+/*
+ * Trie les voitures en fonction de leur temps et du nombre de tour
+ * @pre : carsRace not null
+ * @post : modifie carsRace (nouvel ordre)
+ */
+void sortCarsByCurrTime(structCar carsRace[]);
+
+/*
+ * Lance la course sur carsRace
+ * @pre : carsRace not  null
+ * @post : modifie carsRace
+ */
+void race(int index);
+
+/*
+ * Créé un fichier avec le récap de la course
+ * @pre : carsRace not null
+ * @post : crée un fichier avec le récap de la course
+ */
+void generateRecapFileRace();
+
+#endif /* race_h */
